7c15fc88c2c08c39f53d07ee0db0d9633a34f42c
asakuracl <champi.ludo@hotmail.fr>
2021-02-15 11:02:47 -0300

    add triangle on click on event

--- main.js
+++ main.js
@@ -5,83 +5,35 @@ class myCanvas {
         this.ctx = canvas.getContext('2d');
         canvas.width = "500";
         canvas.height = "500";
-        
-        for (let i = 0; i < 50; i++){
-            this.moveX = Math.floor(Math.random()*500);
-            this.moveY = Math.floor(Math.random()*500);
+
+        //
+      
+
+        //listener on click bouton
+        this.clickOnCanvas = document.querySelector("canvas");
+        this.clickOnCanvas.addEventListener("click", this.clickCanvasEffect.bind(this));
+
+    }
+    // event on click
+    clickCanvasEffect(){
+    
+        for (let i = 0; i < 50; i++) {
+            this.moveX = Math.floor(Math.random() * canvas.width);
+            this.moveY = Math.floor(Math.random() * canvas.height);
+            this.color = ['red']
 
             this.ctx.beginPath();
             this.ctx.moveTo(0 + this.moveX, 10 + this.moveY);
             this.ctx.lineTo(20 + this.moveX, 0 + this.moveY);
-            this.ctx.lineTo(20 + this.moveX, 20+ this.moveY);
+            this.ctx.lineTo(20 + this.moveX, 20 + this.moveY);
 
             this.ctx.closePath()
             this.ctx.lineWidth = 1;
             this.ctx.strokeStyle = '#0f4c81';
             this.ctx.stroke();
         }
-        // this.image = image;
-        // this.variable = 0;
-        // this.dist = -100;
-        // this.temps = 5000;
-        // this.interv = null;
-        // this.demarre = false;
-        // this.lancerDiap();
-
-        // //listener on click bouton
-        // this.buttonLeft = document.querySelector("#leftArrow");
-        // this.buttonLeft.addEventListener("click", this.buttonPrev.bind(this));
-
-        // this.buttonRight = document.querySelector("#rightArrow");
-        // this.buttonRight.addEventListener("click", this.buttonNext.bind(this));
-
-        // this.buttonPaus = document.querySelector("#buttonPaus");
-        // this.buttonPaus.addEventListener("click", this.buttonStop.bind(this));
-
-        // //listener on keyboard
-        // document.addEventListener("keydown", this.keyboard.bind(this));
     }
 
-    // // Change image with click on button
-    // buttonNext() {
-    //     this.variable++;
-    //     if (this.variable > this.container.length - 1) {
-    //         this.variable = 0;
-    //     }
-    //     image.style.marginLeft = -Math.abs((this.variable * this.dist)) + "%";
-    // }
-
-    // buttonPrev() {
-    //     this.variable--;
-    //     if (this.variable < 0) {
-    //         this.variable = this.container.length - 1;
-    //     }
-    //     image.style.marginLeft = -Math.abs((this.variable * this.dist)) + "%";
-    // }
-
-    // buttonStop() {
-    //     if (this.demarre) {
-    //         this.interv = setInterval(this.buttonNext.bind(this), this.temps);
-
-    //     } else {
-    //         clearInterval(this.interv);
-    //     }
-    //     this.demarre = !this.demarre;
-    // }
-
-    // // Change image with keyboard
-    // keyboard(e) {
-    //     if (e.keyCode === 39) {
-    //         this.buttonNext();
-    //     } else if (e.keyCode === 37) {
-    //         this.buttonPrev();
-    //     }
-    // };
-
-    // //change image auto with interval 
-    // lancerDiap() {
-    //     this.interv = setInterval(this.buttonNext.bind(this), this.temps);
-    // }
 }
 
 const canvas = document.querySelector("#myCanvas");
