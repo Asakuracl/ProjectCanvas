3de6e370fa358f9e6afe17fcabafedd22e258c0b
asakuracl <champi.ludo@hotmail.fr>
2021-02-15 23:40:58 -0300

    listener on load instead click change function name to animate some cleane

--- main.js
+++ main.js
@@ -5,54 +5,34 @@ class myCanvas {
         this.ctx = canvas.getContext('2d');
         canvas.width = "500";
         canvas.height = "500";
-        // this.moveX = Math.floor(Math.random() * canvas.width);
-        // this.moveY = Math.floor(Math.random() * canvas.height);
-        this.moveX = 0;
-        this.moveY = 10;
+        this.moveX = Math.floor(Math.random() * canvas.width);
+        this.moveY = Math.floor(Math.random() * canvas.height);
         this.speed = 1;
         this.triangleLength = 20;
 
-        //listener on click bouton
-        this.clickOnCanvas = document.querySelector("canvas");
-        this.clickOnCanvas.addEventListener("click", this.clickCanvasEffect.bind(this));
-   
+        //listener on load
+        window.addEventListener("load", this.animate.bind(this));
     }
-    // event on click
-    clickCanvasEffect(){
-        requestAnimationFrame(this.clickCanvasEffect.bind(this));
-        this.ctx.clearRect(0, 0, canvas.height, canvas.width);
 
+    // event on click
+    animate(){
+        requestAnimationFrame(this.animate.bind(this));
+        this.ctx.clearRect(0, 0, canvas.height, canvas.width);
         this.ctx.beginPath();
         this.ctx.moveTo(0 + this.moveX, 10 + this.moveY);
         this.ctx.lineTo(20 + this.moveX, 0 + this.moveY);
         this.ctx.lineTo(20 + this.moveX, 20 + this.moveY);
-
         this.ctx.closePath()
         this.ctx.lineWidth = 1;
         this.ctx.strokeStyle = '#0f4c81';
         this.ctx.stroke();
 
-        this.moveX += this.speed;
-        this.moveY += this.speed;
-        (this.moveX + this.triangleLength > canvas.width || this.moveX < 0) ? this.speed = -this.speed : this.speed;
+        (this.moveX + this.triangleLength > canvas.width || this.moveX < 0) ? this.speed = -this.speed: this.speed;
         (this.moveY + this.triangleLength > canvas.height || this.moveY < 0) ? this.speed = -this.speed : this.speed;
    
-        
-        // for (let i = 0; i < 50; i++) {
-        //     this.moveX = Math.floor(Math.random() * canvas.width);
-        //     this.moveY = Math.floor(Math.random() * canvas.height);
-        //     this.color = ['red']
+        this.moveX += this.speed;
+        this.moveY += this.speed;
 
-        //     this.ctx.beginPath();
-        //     this.ctx.moveTo(0 + this.moveX, 10 + this.moveY);
-        //     this.ctx.lineTo(20 + this.moveX, 0 + this.moveY);
-        //     this.ctx.lineTo(20 + this.moveX, 20 + this.moveY);
-
-        //     this.ctx.closePath()
-        //     this.ctx.lineWidth = 1;
-        //     this.ctx.strokeStyle = '#0f4c81';
-        //     this.ctx.stroke();
-        // }
     }
 
 }
